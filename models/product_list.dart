265b9116d7c984a8a7b1249bbb2826c7f75113d7
vinicius reis <104855899+ViniciusReisCosta@users.noreply.github.com>
2025-08-14 12:34:55 -0300

    Add files via upload

---
+++ models/product_list.dart
@@ -0,0 +1,135 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/date/dummy_data.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/utils/constants.dart';
+
+class ProductList with ChangeNotifier {
+  final _baseUrl = Constants.productBaseUrl;
+
+  final List<Product> _items = dummyProducts;
+
+  List<Product> get items => [..._items];
+  List<Product> get favoriteItems =>
+      _items.where((prod) => prod.isFavorite).toList();
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  Future<void> loadProducts() async {
+    _items.clear();
+    final response = await http.get(Uri.parse(_baseUrl));
+    print(jsonDecode(response.body));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((ProductId, ProductData) {
+      _items.add(
+        Product(
+          id: ProductId,
+          name: ProductData['name'],
+          description: ProductData['description'],
+          price: ProductData['price'],
+          imageUrl: ProductData['imageUrl'],
+          isFavorite: ProductData['isFavorite'],
+        ),
+      );
+    });
+    notifyListeners();
+  }
+
+  Future<void> saveProduct(Map<String, Object> data) {
+    bool hasId = data['id'] != null;
+
+    final product = Product(
+      id: hasId ? data['id'] as String : Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
+    );
+
+    if (hasId) {
+      return updateProduct(product);
+    } else {
+      return addProduct(product);
+    }
+  }
+
+  Future<void> addProduct(Product product) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl.json'),
+      body: jsonEncode({
+        "name": product.name,
+        "description": product.description,
+        "price": product.price,
+        "imageUrl": product.imageUrl,
+        "isFavorite": product.isFavorite,
+      }),
+    );
+
+    final id = jsonDecode(response.body)['name'];
+    _items.add(
+      Product(
+        id: id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        isFavorite: product.isFavorite,
+      ),
+    );
+    notifyListeners();
+  }
+
+  Future<void> updateProduct(Product product) async {
+    int index = _items.indexWhere((p) => p.id == product.id);
+
+    if (index >= 0) {
+
+      
+      final response = await http.patch(
+      Uri.parse('$_baseUrl/${product.id}.json'),
+      body: jsonEncode({
+        "name": product.name,
+        "description": product.description,
+        "price": product.price,
+        "imageUrl": product.imageUrl,
+      }),
+    );
+      _items[index] = product;
+      notifyListeners();
+    }
+
+    return Future.value();
+  }
+
+  void removeProduct(Product product) {
+    int index = _items.indexWhere((p) => p.id == product.id);
+
+    if (index >= 0) {
+      _items.removeWhere((p) => p.id == product.id);
+      notifyListeners();
+    }
+  }
+}
+
+// bool _showFavoriteOnly = false;
+
+//   List<Product> get items {
+//     if (_showFavoriteOnly) {
+//       return _items.where((prod) => prod.isFavorite).toList();
+//     }
+//     return [..._items];
+//   }
+
+//   void showFavoriteOnly() {
+//     _showFavoriteOnly = true;
+//     notifyListeners();
+//   }
+
+//   void showAll() {
+//     _showFavoriteOnly = false;
+//     notifyListeners();
+//   }
---
+++ utils/constants.dart
@@ -0,0 +1,5 @@
+// TODO Implement this library.
+class Constants {
+  static const productBaseUrl =
+      'https://shop-cod3-138f0-default-rtdb.firebaseio.com/base';
+}
